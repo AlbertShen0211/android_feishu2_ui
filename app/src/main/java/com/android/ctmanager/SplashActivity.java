d0e0893c9091abe79ed788c3068eda7cdab0c29e
22010563_申培保 <shenpeibao@haier.com>
2024-12-17 15:44:07 +0800

    splash

--- app/src/main/java/com/android/ctmanager/SplashActivity.java
+++ app/src/main/java/com/android/ctmanager/SplashActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
